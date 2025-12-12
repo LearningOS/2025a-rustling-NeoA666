9ba53865f06d5d185f261ae424a9701380e92f83
Zhou Zhuojiang <zhouzhuojiang@gmail.com>
2025-12-12 11:03:44 +0800

    update

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -1,24 +1,23 @@
 // quiz2.rs
 //
-// This is a quiz for the following sections:
-// - Strings
-// - Vecs
-// - Move semantics
-// - Modules
-// - Enums
+// 这是关于以下章节的小测验：
+// - 字符串（Strings）
+// - 向量（Vecs）
+// - 移动语义（Move semantics）
+// - 模块（Modules）
+// - 枚举（Enums）
 //
-// Let's build a little machine in the form of a function. As input, we're going
-// to give a list of strings and commands. These commands determine what action
-// is going to be applied to the string. It can either be:
-// - Uppercase the string
-// - Trim the string
-// - Append "bar" to the string a specified amount of times
-// The exact form of this will be:
-// - The input is going to be a Vector of a 2-length tuple,
-//   the first element is the string, the second one is the command.
-// - The output element is going to be a Vector of strings.
+// 让我们用函数的形式构建一个小机器。作为输入，我们将提供一个字符串和命令的列表。
+// 这些命令决定了将对字符串应用什么操作。可以是：
+// - 将字符串转换为大写
+// - 修剪字符串（去除首尾空白）
+// - 将 "bar" 附加到字符串指定的次数
+// 具体形式如下：
+// - 输入将是一个由长度为2的元组组成的向量，
+//   第一个元素是字符串，第二个元素是命令。
+// - 输出将是一个字符串向量。
 //
-// No hints this time!
+// 这次没有提示！
 
 // I AM NOT DONE
 
@@ -32,11 +31,26 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String,Command)>) -> Vec<String>{
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            match command {
+                Command::Uppercase => {
+                    output.push(string.to_uppercase());
+                }
+                Command::Trim => {
+                    output.push(string.trim().to_string());
+                }
+                Command::Append(usize) => {
+                    let mut ans = String::new();
+                    for i in 0..*usize {
+                        ans += &string.clone();
+                    }
+                    output.push(format!("{}bar", ans));
+                }
+            }
         }
         output
     }
@@ -45,7 +59,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
